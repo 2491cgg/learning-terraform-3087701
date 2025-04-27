4cd2c13907995f17c9e40d7cb541341b8ecc3e5b
2491cgg <2491cgg@gmail.com>
2025-04-27 18:30:40 +0530

    error ressoled

--- main.tf
+++ main.tf
@@ -49,17 +49,17 @@ resource "aws_instance" "blog" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [ module.blog_sg.security_group_id ]
 
   target_groups = [
     {
-      name_prifix = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type     = "instance"
+      name_prifix       = "blog-"
+      backend_protocol  = "HTTP"
+      backend_port      = 80
+      target_type       = "instance"
       targets = {
         my_target = {
           target_id = aws_instance.blog.id
@@ -71,10 +71,10 @@ module "alb" {
 
  listeners = [
     {
-      port     = 80
-      protocol = "HTTP"
+      port                = 80
+      protocol            = "HTTP"
       default_action_type = "forward"
-      target_group_index = 0
+      target_group_index  = 0
     }
   ]
 
@@ -84,15 +84,15 @@ module "alb" {
 }
 
 module "blog_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name = "blog"
+  name    = "blog"
 
   vpc_id = module.blog_vpc.vpc_id
 
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
