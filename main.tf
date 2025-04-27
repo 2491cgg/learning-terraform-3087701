b992d3cc6c1189e96d1d11b9accaf32fa2ffc965
2491cgg <2491cgg@gmail.com>
2025-04-27 18:54:53 +0530

    error issue resolve

--- main.tf
+++ main.tf
@@ -87,6 +87,9 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
+
+  load_balancer_type = "application"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [ module.blog_sg.security_group_id ]
@@ -103,7 +106,12 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      target_groups = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
   }
 
   tags = {
