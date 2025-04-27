e7b9120c65cd60abebc45fa780b83c1de6709c1a
2491cgg <2491cgg@gmail.com>
2025-04-27 18:25:47 +0530

    added load balancer module

--- main.tf
+++ main.tf
@@ -46,10 +46,47 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog" {
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prifix = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type     = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
+    }
+  ]
+
+ listeners = [
+    {
+      port     = 80
+      protocol = "HTTP"
+      default_action_type = "forward"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name = "blog_new"
+  name = "blog"
 
   vpc_id = module.blog_vpc.vpc_id
 
